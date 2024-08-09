8d5e17371ecff4c1d9f8930197bd597da45397ea
Rob Simpson <Robert.Simpson@usda.gov>
2024-08-09 09:23:16 -0400

    End of section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,28 +1,31 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AccountService } from '../_services/account.service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive, TitleCasePipe],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrl: './nav.component.css',
 })
 export class NavComponent {
   accountService = inject(AccountService);
   model: any = {};
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: data => {
-        console.log(data);
-      },
-      error: err => {
-        console.log(err);
-      }
-    })
+      next: () => this.router.navigateByUrl('/members'),
+      error: (err) => this.toastr.error(err.error),
+    });
   }
 
   logout() {
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -13,6 +14,7 @@ export class RegisterComponent {
   model: any = {};
   cancelRegister = output<boolean>();
   private accountService = inject(AccountService);
+  private toastr = inject(ToastrService);
 
   register() {
     this.accountService.register(this.model).subscribe({
@@ -21,7 +23,7 @@ export class RegisterComponent {
         this.cancel();
       },
       error: err => {
-        console.error(err);
+        this.toastr.error(err.error);
       }
     })
   }
