97da1a2edd745fcb984e7c173671d400c5d49296
Rob Simpson <Robert.Simpson@usda.gov>
2024-10-08 16:42:22 -0400

    End of Section 10

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 
 import { MembersService } from '../../_services/members.service';
-import { Member } from '../../_models/member';
 
 import { MemberCarComponent } from "../member-card/member-card.component";
 
@@ -13,16 +12,13 @@ import { MemberCarComponent } from "../member-card/member-card.component";
   styleUrl: './member-list.component.css'
 })
 export class MemberListComponent implements OnInit {
-  private memberService = inject(MembersService);
-  members: Member[] = [];
+  memberService = inject(MembersService);
 
   ngOnInit(): void {
-    this.loadMembers();
+    if (this.memberService.members().length === 0) this.loadMembers();
   }
 
   loadMembers() {
-    this.memberService.getMembers().subscribe({
-      next: members => this.members = members
-    })
+    this.memberService.getMembers()
   }
 }
